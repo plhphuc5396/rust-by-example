67770f2e34bc19f450987e51813848c4e21f5c99
Percy Pham <plhphuc@gmail.com>
2023-01-12 17:55:03 +0700

    print_display/testcase_list.html

--- display.rs
+++ display.rs
@@ -14,8 +14,36 @@ impl fmt::Display for Complex {
     }
 }
 
+// Define a structure named `List` containing a `Vec`.
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Extract the value using tuple indexing,
+        // and create a reference to `vec`.
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        // Iterate over `v` in `vec` while enumerating the iteration
+        // count in `count`.
+        for (count, v) in vec.iter().enumerate() {
+            // For every element except the first, add a comma.
+            // Use the ? operator to return on errors.
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{}: {}", count, v)?;
+        }
+
+        // Close the opened bracket and return a fmt::Result value.
+        write!(f, "]")
+    }
+}
+
 fn main() {
     let complex = Complex {real: 3.3, imag: 7.2};
     println!("Display: {}", complex);
     println!("Debug: {:#?}", complex);
+
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
 }
